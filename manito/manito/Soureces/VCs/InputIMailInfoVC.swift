590cb1378f9489c90f76ba9574f918164e86bc7e
rayleigh <rayleigh190@gmail.com>
2023-07-19 22:59:42 +0900

    feat: 메일로 받기 입력 화면  기본 구현

---
+++ manito/manito/Soureces/VCs/InputIMailInfoVC.swift
@@ -0,0 +1,24 @@
+//
+//  InputMailInfoVC.swift
+//  manito
+//
+//  Created by 우진 on 2023/07/18.
+//
+
+import UIKit
+
+class InputMailInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setKeyboardObserver() // 키보드 올라오고 내려갈때 전체 뷰를 키보드 높이 만큼 올렸다 내림
+    }
+    
+    @IBAction func tapBackgroundView(_ sender: Any) {
+        // 배경 터치시 키보드 내리게 함
+        view.endEditing(true)
+    }
+
+}
+
+
