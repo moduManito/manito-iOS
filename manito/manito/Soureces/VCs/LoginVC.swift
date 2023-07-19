62a6e5ef3533fb7fcfd54168072a50311dca0eb3
rayleigh <rayleigh190@gmail.com>
2023-07-19 23:13:10 +0900

    feat: 로그인 화면에서 키보드에 따라 뷰가 위아래로 움직이도록 함

--- manito/manito/Soureces/VCs/LoginVC.swift
+++ manito/manito/Soureces/VCs/LoginVC.swift
@@ -11,12 +11,17 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setKeyboardObserver() // 키보드 올라오고 내려갈때 전체 뷰를 키보드 높이 만큼 올렸다 내림
        
     }
 
     @IBAction func LoginButton(_ sender: UIButton) {
         performSegue(withIdentifier: "ShowMain", sender: sender)
+    }
+    
+    @IBAction func tapBackgroundView(_ sender: Any) {
+        // 배경 터치시 키보드 내리게 함
+        view.endEditing(true)
     }
     
 }
